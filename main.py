926e8f747e67f9ce07dda0ddf3045e7685c6058d
Dmytro Kozin <dimakoua@gmail.com>
2024-04-12 04:07:06 +0300

    update main login

--- main.py
+++ main.py
@@ -3,23 +3,42 @@ import time
 from telebot import TeleBot
 from dotenv import load_dotenv, find_dotenv
 from pars import get_search_results
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
+import json
 
 load_dotenv(find_dotenv())
 
 bot = TeleBot(os.getenv("TOKEN"))
 
 @bot.message_handler(func=lambda message: True)
-def handle_message(message):
+def handle_message(message, page=1):
     search_term = message.text
     search_results = get_search_results(search_term)
-    
-    if search_results:
-        for result in search_results:
-            time.sleep(2)
-            bot.send_message(message.chat.id, result)
-    else:
-        bot.send_message(message.chat.id, "Ничего не найдено.")
+    print(search_results)
+    markup = InlineKeyboardMarkup()
 
+    button_data = json.dumps({"action": "load_more", "page": page + 1, "search_term": search_term})
+    button = InlineKeyboardButton("Load More", callback_data=button_data)
+    markup.add(button)
+
+    if search_results and len(search_results) > page - 1 and search_results[page - 1]:
+        time.sleep(2)
+        bot.send_message(message.chat.id, search_results[page - 1], reply_markup=markup)
+    else:
+        bot.send_message(message.chat.id, "Not Found... Please adjust your query and try again")
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_query(call):
+    decoded_data = json.loads(call.data)
+
+    action = decoded_data.get("action", "default_action") 
+    page = decoded_data.get("page", 1)
+    search_term = decoded_data.get("search_term", "")
+
+    if action == "load_more":
+        original_message = bot.send_message(call.message.chat.id, "Loading...")
+        original_message.text = search_term
+        handle_message(original_message, page=page)
 if __name__ == "__main__":
     bot.infinity_polling()
 
@@ -101,4 +120,3 @@ if __name__ == "__main__":
 #         botTimeWeb.send_message(message.chat.id, second_mess)
 
 # botTimeWeb.infinity_polling()
-
