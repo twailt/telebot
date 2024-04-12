1ecae6d706af7616335e6d052547046ab4d7e59a
twailt <bohdankozin@gmail.com>
2024-04-12 13:35:26 +0300

    работает

--- main.py
+++ main.py
@@ -3,102 +3,42 @@ import time
 from telebot import TeleBot
 from dotenv import load_dotenv, find_dotenv
 from pars import get_search_results
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
+import json
 
 load_dotenv(find_dotenv())
 
 bot = TeleBot(os.getenv("TOKEN"))
 
 @bot.message_handler(func=lambda message: True)
-def handle_message(message):
+def handle_message(message, page=1):
     search_term = message.text
     search_results = get_search_results(search_term)
-    
-    if search_results:
-        for result in search_results:
-            time.sleep(2)
-            bot.send_message(message.chat.id, result)
+    print(search_results)
+    markup = InlineKeyboardMarkup()
+
+    button_data = f'load_more|{page + 1}|{search_term}'
+    button = InlineKeyboardButton("Load More", callback_data=button_data)
+    markup.add(button)
+
+    if search_results and len(search_results) > page - 1 and search_results[page - 1]:
+        bot.send_message(message.chat.id, search_results[page - 1], reply_markup=markup)
     else:
-        bot.send_message(message.chat.id, "Ничего не найдено.")
+        bot.send_message(message.chat.id, "Not Found... Please adjust your query and try again")
+        
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_query(call):
+    decoded_data = call.data.split('|')
+
+    action = decoded_data[0]
+    page = int(decoded_data[1])
+    search_term = decoded_data[2]
+
+    if action == "load_more":
+        original_message = bot.send_message(call.message.chat.id, "Loading...")
+        original_message.text = search_term
+        handle_message(original_message, page=page)
 
 if __name__ == "__main__":
     bot.infinity_polling()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# from telebot import types
-# import telebot
-# import os
-# from dotenv import load_dotenv, find_dotenv
-# # from pars import search_term
-
-# load_dotenv(find_dotenv())
-
-# botTimeWeb = telebot.TeleBot(os.getenv("TOKEN"))
-# @botTimeWeb.message_handler(commands=['start'])
-# def startBot(message):
-#     first_mess = f"<b>{message.from_user.first_name} {message.from_user.last_name}</b>, привет!\nХочешь найти фильм?"
-#     markup = telebot.types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
-#     button_yes = telebot.types.KeyboardButton(text='Да')
-#     button_no = telebot.types.KeyboardButton(text='Нет')
-
-#     markup.add(button_yes, button_no)
-#     botTimeWeb.send_message(message.chat.id, first_mess, parse_mode='html', reply_markup=markup)
-
-# @botTimeWeb.message_handler(func=lambda message: True)
-# def response(message):
-#     if message.text == "Да":
-#         second_mess = "Веди что хочешь найти!"
-#         markup = types.InlineKeyboardMarkup()
-#         markup.add(types.InlineKeyboardButton("Перейти на сайт", url="https://animego.org/"))
-#         botTimeWeb.send_message(message.chat.id, second_mess, reply_markup=markup)
-#         botTimeWeb.answer_callback_query(message.id)
-        
-#         botTimeWeb.send_message(message.chat.id, second_mess)
-#     elif message.text == "Нет":
-#         second_mess = "До следующей встречи"
-#         botTimeWeb.send_message(message.chat.id, second_mess)
-
-# botTimeWeb.infinity_polling()
-
--- pars.py
+++ pars.py
@@ -6,98 +6,22 @@ def get_search_results(search_term):
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
     }
-    data = {
-        'q': search_term,
-        'scf': 'fx',
-        'search_start': 0,
-        'do': 'search',
-        'subaction': 'search',
-        'years_ot': 1902,
-        'years_do': 2024,
-        'kpi_ot': 1,
-        'kpi_do': 10,
-        'imdb_ot': 1,
-        'imdb_do': 10,
-        'sort_name': '',
-        'undefined': 'asc',
-        'sort_date': '',
-        'sort_favorite': '',
-        'simple': 1
-    }
 
-    response = requests.post(base_url, headers=headers, data=data)
-    soup = bs(response.text, 'html.parser')
+    all_links = []
+    page = 1
+    while True:
+        url = f"{base_url}?do=search&subaction=search&q={search_term}&page={page}"
+        response = requests.get(url=url, headers=headers)
+        soup = bs(response.text, 'html.parser')
 
-    navigation_div = soup.find("div", class_="b-navigation")
-    if navigation_div:
-        page_count = navigation_div.find_all("a")[-2].text
-        all_links = []
-        for i in range(1, int(page_count) + 1):
-            url = f"https://rezka.ag/search/?do=search&subaction=search&q={search_term}&page={i}"
-            r = requests.get(url=url, headers=headers)
-            soup_page = bs(r.text, 'html.parser') 
+        all_films = soup.find_all("div", class_="b-content__inline_item")
+        if not all_films:
+            break
 
-            all_films = soup_page.find_all("div", class_="b-content__inline_item")
-            for film in all_films:
-                link = film.find("a")["href"]
-                all_links.append(link)
+        for film in all_films:
+            link = film.find("a")["href"]
+            all_links.append(link)
 
-        return all_links
-    else:
-        return []
+        page += 1
 
-
-
-
-
-
-
-
-
-
-
-# # from bs4 import BeautifulSoup
-# # import requests
-
-# # url = '' 
-# # page = requests.get(url)
-# # print(page.status_code)
-
-# # soup = BeautifulSoup(page.content, "html.parser")
-
-# # main_id = soup.find(id="main")
-# # links_a_main = main_id.find_all("a")
-
-# # with open("ukr_net_links.txt", "w", encoding="utf-8") as file:
-# #     for link in links_a_main:
-# #         # Преобразовываем ссылку в строку с помощью prettify() и записываем в файл
-# #         file.write(link.prettify() + "\n")
-
-
-
-# # from bs4 import BeautifulSoup
-# # import requests
-# # import json
-
-# # url = '' 
-# # page = requests.get(url)
-# # response = requests.get(url)
-# # print(page.status_code)
-
-# # soup = BeautifulSoup(page.content, "html.parser")
-
-# # main_id = soup.find(id="main")
-# # links_a_main = main_id.find_all("a")
-
-# # new_list = [str(i) for i in links_a_main]
-
-# # with open ("ukr_net_links.txt", "w", encoding='utf-8') as file:
-# #         for i in new_list:
-# #             file.write("\n" + i)
-
-
-
-# # название 
-# # рік 
-# # описание 
-# # картінка 
+    return all_links
